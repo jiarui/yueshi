98b61bb2c4bd91494ae5f6f7bd489113a0f35f4d
Jia Rui <jr.coaxmetal@gmail.com>
2024-05-20 15:57:41 +0800

    under dev

--- src/lua/lex.cpp
+++ src/lua/lex.cpp
@@ -1,5 +1,9 @@
 #include "lua/lex.h"
 using namespace peg;
-Rule<> chunk;
-auto whitespaces = *terminal(std::set({' ', '\f', '\t', '\v'}));
+auto WS = *terminal(std::set({' ', '\f', '\t', '\v'}));
 auto linebreaks = terminal('\n');
+auto identifiers = terminal<char>([](char c){return std::isalpha(c);});
+
+next(Context)
+//while context.ended()
+
